549b5a2a9a44cf3cdc0c8bf504885c49f451b3af
Michele Mazzucco <mmazzucco@demonware.net>
2014-05-26 18:46:00 +0200

    adding test for power and more methods

--- src/math/Power.java
+++ src/math/Power.java
@@ -11,7 +11,54 @@ package math;
  */
 public class Power {
 
-    private static int counter = 0;
+    /**
+     * Rounds log2(n) to the next integer.
+     * 
+     * @throws IllegalArgumentException If n &le; 0.
+     */
+    public static int ceilingLog2(int n) throws IllegalArgumentException {
+        checkArg(n);
+        int counter;
+        for (counter = 0; n > 1; n >>= 1, counter++) {
+            ;
+        }
+        return counter;
+    }
+
+
+    /**
+     * Computes the the smallest power of 2 larger than <i>n</i>
+     * 
+     * @throws IllegalArgumentException If n &le; 0.
+     */
+    public static int ceilingPow2(int n) throws IllegalArgumentException {
+        checkArg(n);
+        while (!isPowerOf2(n)) {
+            n++;
+        }
+        return n;
+    }
+
+
+    private static final void checkArg(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n should be > 0");
+        }
+    }
+
+
+    /**
+     * Checks if <i>n</i> is a power of 2.
+     * 
+     * @return <code>true</code> if <i>n</i> is a power of 2, <code>false</code>
+     *         otherwise.
+     * @throws IllegalArgumentException If n &le; 0.
+     */
+    public static boolean isPowerOf2(int n) throws IllegalArgumentException {
+        checkArg(n);
+        return (n & (n - 1)) == 0;
+    }
+
 
     /**
      * Computes pow(num, exp) without using the pow function in O(log2(exp))
@@ -20,47 +67,47 @@ public class Power {
      * @param base The base.
      * @param exp The exponent (>= 0).
      * @return num ^ pow.
-     * @throws IllegalArgumentException If pow is negative.
+     * @throws IllegalArgumentException If exp is negative.
      */
     public static long power(long base, long exp) {
-	if (exp < 0) {
-	    throw new IllegalArgumentException("pow should be positive!");
-	}
-	counter++;
-	if (exp == 1) {
-	    return base;
-	}
-	if (exp == 0) {
-	    return 1;
-	}
-	if ((exp & (exp - 1)) == 0) { // power of 2
-	    final long val = power(base, exp >>> 1);
-	    return val * val;
-	}
-	final long val = power(base, exp >>> 1);
-	return base * val * val;
+        if (exp < 0) {
+            throw new IllegalArgumentException("pow should be positive!");
+        }
+        if (exp == 1 || base == 1) {
+            return base;
+        }
+        if (exp == 0) {
+            return 1;
+        }
+        if ((exp & (exp - 1)) == 0) { // power of 2
+            final long val = power(base, exp >>> 1);
+            return val * val;
+        }
+        final long val = power(base, exp >>> 1);
+        return base * val * val;
     }
+
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-	int base = 2;
-	int pow = -3;
+        int base = 2;
+        int pow = -3;
 
-	try {
-	    power(base, pow);
-	    throw new IllegalStateException("expecting expection here");
-	} catch (IllegalArgumentException expected) {
-	    // expected
-	}
-	
-	
-	pow = 3;
-	long res = power(base, pow);
-	
-	System.out.println(res);
-	System.out.println(counter);
+        try {
+            power(base, pow);
+            throw new IllegalStateException("expecting expection here");
+        } catch (IllegalArgumentException expected) {
+            // expected
+        }
+
+        pow = 3;
+        long res = power(base, pow);
+
+        System.out.println(res);
+
+        System.out.println(ceilingLog2(0));
     }
 
 }
---
+++ test/math/PowerTest.java
@@ -0,0 +1,125 @@
+/**
+ * 
+ */
+package math;
+
+import static org.junit.Assert.*;
+import static math.Power.*;
+
+import org.junit.Test;
+
+/**
+ * @author michelemazzucco
+ */
+public class PowerTest {
+
+    /**
+     * Test method for {@link math.Power#ceilingLog2(int)}.
+     */
+    @Test
+    public final void testCeilingLog2() {
+        int val = ceilingLog2(1);
+        assertEquals(0, val);
+        val = ceilingLog2(2);
+        assertEquals(1, val);
+    }
+
+
+    /**
+     * Test method for {@link math.Power#ceilingLog2(int)}.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public final void testCeilingLog2Zero() {
+        ceilingLog2(0);
+    }
+
+
+    /**
+     * Test method for {@link math.Power#ceilingPow2(int)}.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public final void testCeilingPow2Zero() {
+        ceilingPow2(0);
+    }
+
+
+    /**
+     * Test method for {@link math.Power#ceilingPow2(int)}.
+     */
+    @Test
+    public final void testCeilingPow2() {
+        int val = ceilingPow2(1);
+        assertEquals(1, val);
+    }
+
+
+    /**
+     * Test method for {@link math.Power#isPowerOf2(int)}.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public final void testIsPowerOf2Zero() {
+        isPowerOf2(0);
+    }
+
+
+    /**
+     * Test method for {@link math.Power#isPowerOf2(int)}.
+     */
+    @Test
+    public final void testIsPowerOf2One() {
+        assertTrue(isPowerOf2(1));
+    }
+
+
+    /**
+     * Test method for {@link math.Power#isPowerOf2(int)}.
+     */
+    @Test
+    public final void testIsPowerOf2() {
+        assertTrue(isPowerOf2(2));
+    }
+
+
+    /**
+     * Test method for {@link math.Power#isPowerOf2(int)}.
+     */
+    @Test
+    public final void testIsPowerOf2False() {
+        assertFalse(isPowerOf2(3));
+    }
+    
+    /**
+     * Test method for {@link math.Power#power(long, long)}.
+     */
+    @Test
+    public final void testPowerOne() {
+        assertEquals(1, power(1, 100));
+    }
+    
+    /**
+     * Test method for {@link math.Power#power(long, long)}.
+     */
+    @Test
+    public final void testPowerZero() {
+        assertEquals(1, power(100, 0));
+    }
+
+
+    /**
+     * Test method for {@link math.Power#power(long, long)}.
+     */
+    @Test
+    public final void testPower() {
+        int base = 3;
+        int exp = 2;
+        assertEquals(9, power(base, exp));
+
+        base = 2;
+        exp = 3;
+        assertEquals(8, power(base, exp));
+        
+        base = -2;
+        assertEquals(-8, power(base, exp));
+    }
+
+}
